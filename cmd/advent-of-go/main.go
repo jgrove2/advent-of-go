674affd48c2993d963b336d5113d2ae309016f93
jgrove2 <26723005+jgrove2@users.noreply.github.com>
2023-12-03 22:49:55 -0500

    Day 3

--- cmd/advent-of-go/main.go
+++ cmd/advent-of-go/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/jgrove2/advent-of-go/internal/day1"
 	"github.com/jgrove2/advent-of-go/internal/day2"
+	"github.com/jgrove2/advent-of-go/internal/day3"
 )
 
 func main() {
 	fmt.Println("Running Advent of code")
 	day1.Day1()
 	day2.Day2()
+	day3.Day3()
 }
